c8ca09bb9ca6731e10191f2b8420166f032aca84
nightkillerisded <91191866+nightkillerisded@users.noreply.github.com>
2021-10-02 15:04:53 +0530

    Create hallow.py

---
+++ hallow.py
@@ -0,0 +1,11 @@
+import pyautogui
+import time
+import random
+
+time.sleep(10)
+
+spamtext = "text"
+for  in range(100):
+    pyautogui.typewrite(spam_text)
+    pyautogui.press('enter')
+    time.sleep(0.7)
